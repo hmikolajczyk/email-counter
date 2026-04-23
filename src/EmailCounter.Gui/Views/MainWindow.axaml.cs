20576afcd136fdfb4fa4c85c0c201a4f5c055d9e
dianthusie <haniamikola@gmail.com>
2026-04-24 01:31:22 +0200

    add gui

--- src/EmailCounter.Gui/Views/MainWindow.axaml.cs
+++ src/EmailCounter.Gui/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using EmailCounter.Gui.ViewModels;
 
 namespace EmailCounter.Gui.Views;
 
@@ -7,5 +8,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainWindowViewModel();
     }
 }
