d55158fc07240e027d9aea0c7cc86ef8b3eaa8f3
dianthusie <haniamikola@gmail.com>
2026-05-16 03:48:20 +0200

    test: setup xUnit, Moq and Coverlet with initial ViewModel test

---
+++ src/EmailCounter.Tests/StatusTests.cs
@@ -0,0 +1,19 @@
+﻿using EmailCounter.Gui.ViewModels;
+using Xunit;
+
+namespace EmailCounter.Tests;
+
+public class StatusTests
+{
+    [Fact]
+    public void ResetStatus_ShouldSetDefaultValues()
+    {
+        var vm = new MainWindowViewModel();
+        vm.StatusMessage = "ERROR";
+        vm.StatusColor = "Red";
+        vm.SelectedFolder = null; 
+
+        Assert.Equal("", vm.StatusMessage);
+        Assert.Equal("Gray", vm.StatusColor);
+    }
+}
